1208455894b55ffee893f5ce9419423640404a47
Navid Aghajari <navid.ajr28@gmail.com>
2021-12-08 14:09:38 +0330

    Fix evakuator bug

--- MathExpressionEvaluator/ExpressionEvaluator.cs
+++ MathExpressionEvaluator/ExpressionEvaluator.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using MathProject.DataLayer.Entities;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 
 namespace MathExpressionEvaluator
 {
@@ -22,161 +23,228 @@ namespace MathExpressionEvaluator
             if (string.IsNullOrWhiteSpace(expression))
                 throw new Exception("Expression can not be empty.");
 
-
             expression = StringTrimmer(expression);
             var expressionClone = expression;
 
-
-            int? indexOfDivide = expression.IndexOf('/');
-            int? indexOfMultiple = expression.IndexOf('*');
-            if (indexOfMultiple < indexOfDivide)
+            
+            while (expressionClone.Contains("*") || expressionClone.Contains("/"))
             {
-                var highPriorityValues1 = expression.Split(new char[] { '+', '-', '/' });
-                foreach (var value in highPriorityValues1)
+
+                int indexOfDivide = expressionClone.IndexOf('/');
+                int indexOfMultiple = expressionClone.IndexOf('*');
+
+                if ( indexOfMultiple > 0 &&  (indexOfMultiple < indexOfDivide || (indexOfDivide < 0 && indexOfMultiple > 0)))
                 {
-                    if (value.Contains('*'))
-                    {
-                        var items = value.Split('*');
-                        double result = 1;
-                        foreach (string item in items)
-                        {
-                            result *= double.Parse(item);
-                        }
-                        expressionClone = expressionClone.Replace($"{value}", $"{result}");
-                    }
+                    var leftElementString = LeftElementGenerator(expressionClone, indexOfMultiple);
+                    var leftElement = double.Parse(leftElementString);
+
+                    var rightElementString = RightElementGenerator(expressionClone, indexOfMultiple);
+                    var rightElement = double.Parse(rightElementString);
+                    
+
+                    string value = leftElement + "*" + rightElement;
+                    double result = leftElement * rightElement;
+
+                    expressionClone = expressionClone.Replace($"{value}", $"{result}");
                 }
-
-                var highPriorityValues2 = expressionClone.Split(new char[] { '+', '-', '*' });
-                foreach (var value in highPriorityValues2)
+                else
                 {
-                    if (value.Contains('/'))
-                    {
-                        var items = value.Split('/');
-                        double result = double.Parse(items[0]);
-                        foreach (string item in items)
-                        {
-                            if(item == items[0])
-                                continue;
+                    var leftElementString = LeftElementGenerator(expressionClone, indexOfDivide);
+                    var leftElement = double.Parse(leftElementString);
 
-                            result /= double.Parse(item);
-                        }
-                        expressionClone = expressionClone.Replace($"{value}", $"{result}");
-                    }
-                }
-            }
-            else
-            {
-                var highPriorityValues2 = expressionClone.Split(new char[] { '+', '-', '*' });
-                foreach (var value in highPriorityValues2)
-                {
-                    if (value.Contains('/'))
-                    {
-                        var items = value.Split('/');
-                        double result = double.Parse(items[0]);
-                        foreach (string item in items)
-                        {
-                            if (item == items[0])
-                                continue;
+                    var rightElementString = RightElementGenerator(expressionClone, indexOfDivide);
+                    var rightElement = double.Parse(rightElementString);
 
-                            result /= double.Parse(item);
-                        }
-                        expressionClone = expressionClone.Replace($"{value}", $"{result}");
-                    }
-                }
 
-                var highPriorityValues1 = expressionClone.Split(new char[] { '+', '-', '/' });
-                foreach (var value in highPriorityValues1)
-                {
-                    if (value.Contains('*'))
-                    {
-                        var items = value.Split('*');
-                        double result = 1;
-                        foreach (string item in items)
-                        {
-                            result *= double.Parse(item);
-                        }
-                        expressionClone = expressionClone.Replace($"{value}", $"{result}");
-                    }
+                    string value = leftElement + "/" + rightElement;
+                    double result = leftElement / rightElement;
+
+                    expressionClone = expressionClone.Replace($"{value}", $"{result}");
                 }
             }
 
 
+            //if (indexOfMultiple < indexOfDivide)
+            //{
+            //    var highPriorityValues1 = expression.Split(new char[] { '+', '-', '/' });
+            //    foreach (var value in highPriorityValues1)
+            //    {
+            //        if (value.Contains('*'))
+            //        {
+            //            var items = value.Split('*');
+            //            double result = 1;
+            //            foreach (string item in items)
+            //            {
+            //                result *= double.Parse(item);
+            //            }
+            //            expressionClone = expressionClone.Replace($"{value}", $"{result}");
+            //        }
+            //    }
+
+            //    var highPriorityValues2 = expressionClone.Split(new char[] { '+', '-', '*' });
+            //    foreach (var value in highPriorityValues2)
+            //    {
+            //        if (value.Contains('/'))
+            //        {
+            //            var items = value.Split('/');
+            //            double result = double.Parse(items[0]);
+            //            foreach (string item in items)
+            //            {
+            //                if(item == items[0])
+            //                    continue;
+
+            //                result /= double.Parse(item);
+            //            }
+            //            expressionClone = expressionClone.Replace($"{value}", $"{result}");
+            //        }
+            //    }
+            //}
+            //else
+            //{
+            //    var highPriorityValues2 = expressionClone.Split(new char[] { '+', '-', '*' });
+            //    foreach (var value in highPriorityValues2)
+            //    {
+            //        if (value.Contains('/'))
+            //        {
+            //            var items = value.Split('/');
+            //            double result = double.Parse(items[0]);
+            //            foreach (string item in items)
+            //            {
+            //                if (item == items[0])
+            //                    continue;
+
+            //                result /= double.Parse(item);
+            //            }
+            //            expressionClone = expressionClone.Replace($"{value}", $"{result}");
+            //        }
+            //    }
+
+            //    var highPriorityValues1 = expressionClone.Split(new char[] { '+', '-', '/' });
+            //    foreach (var value in highPriorityValues1)
+            //    {
+            //        if (value.Contains('*'))
+            //        {
+            //            var items = value.Split('*');
+            //            double result = 1;
+            //            foreach (string item in items)
+            //            {
+            //                result *= double.Parse(item);
+            //            }
+            //            expressionClone = expressionClone.Replace($"{value}", $"{result}");
+            //        }
+            //    }
+            //}
 
 
 
-            int? indexOfSum = expressionClone.IndexOf('+');
-            int? indexOfMinus = expressionClone.IndexOf('-');
-            if (indexOfSum < indexOfMinus)
+
+            while (expressionClone.Contains("+") || expressionClone.Contains("-"))
             {
-                var lowPriorityValues1 = expressionClone.Split(new char[] { '*', '/', '-' });
-                foreach (var value in lowPriorityValues1)
+                int indexOfSum = expressionClone.IndexOf('+');
+                int indexOfMinus = expressionClone.IndexOf('-');
+
+                if (indexOfSum > 0 && (indexOfSum < indexOfMinus || (indexOfMinus < 0 && indexOfSum > 0)))
                 {
-                    if (value.Contains('+'))
-                    {
-                        var items = value.Split('+');
-                        double result = 0;
-                        foreach (string item in items)
-                        {
-                            result += double.Parse(item);
-                        }
-                        expressionClone = expressionClone.Replace($"{value}", $"{result}");
-                    }
+                    var leftElementString = LeftElementGenerator(expressionClone, indexOfSum);
+                    var leftElement = double.Parse(leftElementString);
+
+                    var rightElementString = RightElementGenerator(expressionClone, indexOfSum);
+                    var rightElement = double.Parse(rightElementString);
+
+
+                    string value = leftElement + "+" + rightElement;
+                    double result = leftElement + rightElement;
+
+                    expressionClone = expressionClone.Replace($"{value}", $"{result}");
                 }
-
-                var lowPriorityValues2 = expressionClone.Split(new char[] { '*', '/', '+' });
-                foreach (var value in lowPriorityValues2)
+                else
                 {
-                    if (value.Contains('-'))
-                    {
-                        var items = value.Split('-');
-                        double result = double.Parse(items[0]);
-                        foreach (string item in items)
-                        {
-                            if (item == items[0])
-                                continue;
+                    var leftElementString = LeftElementGenerator(expressionClone, indexOfMinus);
+                    var leftElement = double.Parse(leftElementString);
 
-                            result -= double.Parse(item);
-                        }
-                        expressionClone = expressionClone.Replace($"{value}", $"{result}");
-                    }
+                    var rightElementString = RightElementGenerator(expressionClone, indexOfMinus);
+                    var rightElement = double.Parse(rightElementString);
+
+
+                    string value = leftElement + "-" + rightElement;
+                    double result = leftElement - rightElement;
+
+                    expressionClone = expressionClone.Replace($"{value}", $"{result}");
                 }
             }
-            else
-            {
-                var lowPriorityValues2 = expressionClone.Split(new char[] { '*', '/', '+' });
-                foreach (var value in lowPriorityValues2)
-                {
-                    if (value.Contains('-'))
-                    {
-                        var items = value.Split('-');
-                        double result = double.Parse(items[0]);
-                        foreach (string item in items)
-                        {
-                            if (item == items[0])
-                                continue;
 
-                            result -= double.Parse(item);
-                        }
-                        expressionClone = expressionClone.Replace($"{value}", $"{result}");
-                    }
-                }
 
-                var lowPriorityValues1 = expressionClone.Split(new char[] { '*', '/', '-' });
-                foreach (var value in lowPriorityValues1)
-                {
+            //if (indexOfSum < indexOfMinus)
+            //{
+            //    var lowPriorityValues1 = expressionClone.Split(new char[] { '*', '/', '-' });
+            //    foreach (var value in lowPriorityValues1)
+            //    {
+            //        if (value.Contains('+'))
+            //        {
+            //            var items = value.Split('+');
+            //            double result = 0;
+            //            foreach (string item in items)
+            //            {
+            //                result += double.Parse(item);
+            //            }
+            //            expressionClone = expressionClone.Replace($"{value}", $"{result}");
+            //        }
+            //    }
 
-                    if (value.Contains('+'))
-                    {
-                        var items = value.Split('+');
-                        double result = 0;
-                        foreach (string item in items)
-                        {
-                            result += double.Parse(item);
-                        }
-                        expressionClone = expressionClone.Replace($"{value}", $"{result}");
-                    }
-                }
-            }
+            //    var lowPriorityValues2 = expressionClone.Split(new char[] { '*', '/', '+' });
+            //    foreach (var value in lowPriorityValues2)
+            //    {
+            //        if (value.Contains('-'))
+            //        {
+            //            var items = value.Split('-');
+            //            double result = double.Parse(items[0]);
+            //            foreach (string item in items)
+            //            {
+            //                if (item == items[0])
+            //                    continue;
+
+            //                result -= double.Parse(item);
+            //            }
+            //            expressionClone = expressionClone.Replace($"{value}", $"{result}");
+            //        }
+            //    }
+            //}
+            //else
+            //{
+            //    var lowPriorityValues2 = expressionClone.Split(new char[] { '*', '/', '+' });
+            //    foreach (var value in lowPriorityValues2)
+            //    {
+            //        if (value.Contains('-'))
+            //        {
+            //            var items = value.Split('-');
+            //            double result = double.Parse(items[0]);
+            //            foreach (string item in items)
+            //            {
+            //                if (item == items[0])
+            //                    continue;
+
+            //                result -= double.Parse(item);
+            //            }
+            //            expressionClone = expressionClone.Replace($"{value}", $"{result}");
+            //        }
+            //    }
+
+            //    var lowPriorityValues1 = expressionClone.Split(new char[] { '*', '/', '-' });
+            //    foreach (var value in lowPriorityValues1)
+            //    {
+
+            //        if (value.Contains('+'))
+            //        {
+            //            var items = value.Split('+');
+            //            double result = 0;
+            //            foreach (string item in items)
+            //            {
+            //                result += double.Parse(item);
+            //            }
+            //            expressionClone = expressionClone.Replace($"{value}", $"{result}");
+            //        }
+            //    }
+            //}
 
 
             var finalResult = float.Parse(expressionClone);
@@ -202,5 +270,69 @@ namespace MathExpressionEvaluator
             });
             _context.SaveChanges();
         }
+
+        private string LeftElementGenerator(string expression, int index)
+        {
+            var leftElements = new List<string>();
+            for (var i = 1; i < expression.Length; i++)
+            {
+                char item;
+                try
+                {
+                    item = expression.ElementAt(index - i);
+                }
+                catch (System.ArgumentOutOfRangeException)
+                {
+                    break;
+                }
+
+                if (char.IsDigit(item) || item == '.')
+                    leftElements.Add(item.ToString());
+                else
+                    break;
+
+            }
+
+            var leftElement = "";
+            leftElements.Reverse();
+            foreach (var element in leftElements)
+            {
+                leftElement += element;
+            }
+
+            return leftElement;
+        }
+
+        private string RightElementGenerator(string expression, int indexOfMultiple)
+        {
+            var rightElements = new List<string>();
+            for (var i = 1; i < expression.Length; i++)
+            {
+                char item;
+                try
+                {
+                    item = expression.ElementAt(indexOfMultiple + i);
+                }
+                catch (System.ArgumentOutOfRangeException)
+                {
+                    break;
+                }
+
+                if (char.IsDigit(item) || item == '.')
+                    rightElements.Add(item.ToString());
+                else
+                    break;
+
+            }
+
+            var rightElement = "";
+            foreach (var element in rightElements)
+            {
+                rightElement += element;
+            }
+
+            return rightElement;
+        }
     }
 }
+
